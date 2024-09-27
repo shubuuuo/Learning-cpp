89804cf005452a08f78425c7c6fb33129a5a1d74
Shubham Maurya <157304918+shubuuuo@users.noreply.github.com>
2024-09-28 01:36:59 +0530

    Create 02_double.cpp

---
+++ 01_basics/02_variables/02_double.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    double days = 4.5;
+    double price = 21.43;
+    double temperature = 29.32;
+
+    std::cout << "Today is my " << days << "th day." << '\n';
+    std::cout << "And the price is Rs." << price << '\n';
+    std::cout << "Today's Temperature is " << temperature << " degree celcius." << '\n';
+
+    return 0;
+}
