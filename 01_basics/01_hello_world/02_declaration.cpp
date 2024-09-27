3321e153ebdfaaad3195d4f89d5888c376b3c643
Shubham Maurya <157304918+shubuuuo@users.noreply.github.com>
2024-09-28 01:35:13 +0530

    Create 02_declaration.cpp

---
+++ 01_basics/01_hello_world/02_declaration.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    int x; // Declaration
+    x = 5; // Assigngment
+    std::cout << x << '\n';
+
+    // or we can combine them...
+    int y = 6;
+    std::cout << y << '\n';
+
+    int sum = x + y;
+    std::cout << sum << '\n';
+
+    
+    return 0;
+}
