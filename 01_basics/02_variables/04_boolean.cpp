5d97ecd790114e759e23c34f1e8fab7ac40eb4c3
Shubham Maurya <157304918+shubuuuo@users.noreply.github.com>
2024-09-28 01:37:31 +0530

    Add files via upload

---
+++ 01_basics/02_variables/04_boolean.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    bool student = true;
+    if (student)
+    {
+        std::cout << "Sheila is a student.";
+    }
+    else
+    {
+        std::cout << "Sheila is a not a student.";
+    }
+    
+    return 0;
+}
---
+++ 01_basics/02_variables/05_strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    // String is a type of object that represents a sequence of texts.
+
+    std::string name_1 = "Ramesh";
+    std::string name_2 = "Suresh";
+    std::string address = "Dongre Chawl, New Delhi";
+    std::cout << name_1 << " is " << name_2 << "'s friend." << " and they live together at " << address << '\n';
+
+    return 0;
+}
