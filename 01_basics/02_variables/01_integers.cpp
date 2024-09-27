fea525f19a5b70e8c9d989731f38695b71e6a1b1
Shubham Maurya <157304918+shubuuuo@users.noreply.github.com>
2024-09-28 01:36:24 +0530

    Create 01_integers.cpp

---
+++ 01_basics/02_variables/01_integers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    // integers - whole numbers
+    int age = 21;
+    int year = 2024;
+    int days = 4.5;
+
+    std::cout << "Ramesh is " << age << " years old in " << year << " and today is his " << days << "th day.";
+    // Now we have problem that integer can only store one value and not point.
+    return 0;
+}
